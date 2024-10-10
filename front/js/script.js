74583c80fbe4bc132a9e8a47847c18a2a8fc2b99
MATHEUS GIONGO MING <22.00096-8@maua.br>
2024-10-10 12:33:46 -0300

    até form para frontend

--- front/js/script.js
+++ front/js/script.js
@@ -7,5 +7,14 @@ async function obterFilmes()
   const URLcompleta = `${protocolo}${baseURL}${filmesEndPoint}`
   const filmes = (await axios.get(URLcompleta)).data
 
-  console.log(filmes)
+  let tabela = document.querySelector(".filmes")
+  let corpoTabela = tabela.getElementsByTagName("tbody")[0]
+  for (let filme of filmes)
+  {
+    let linha = corpoTabela.insertRow(0)
+    let celulaTitulo = linha.insertCell(0)
+    let celulaSinopse = linha.insertCell(1)
+    celulaTitulo.innerHTML = filme.titulo
+    celulaSinopse.innerHTML = filme.sinopse
+  }
 }

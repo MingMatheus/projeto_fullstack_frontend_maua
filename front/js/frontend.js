4ddbfe0902dd2c62845165db4088f4929cf57de7
MATHEUS GIONGO MING <22.00096-8@maua.br>
2024-10-31 13:07:32 -0300

    novo usuario com alertas ok

--- front/js/frontend.js
+++ front/js/frontend.js
@@ -1,14 +1,11 @@
 const protocolo = "http://"
 const baseURL = "localhost:3000"
-const filmesEndPoint = "/filmes"
 
-async function obterFilmes()
+function exibirFilmes(filmes)
 {
-  const URLcompleta = `${protocolo}${baseURL}${filmesEndPoint}`
-  const filmes = (await axios.get(URLcompleta)).data
-
   let tabela = document.querySelector(".filmes")
   let corpoTabela = tabela.getElementsByTagName("tbody")[0]
+  corpoTabela.innerHTML = ""
   for (let filme of filmes)
   {
     let linha = corpoTabela.insertRow(0)
@@ -19,8 +16,32 @@ async function obterFilmes()
   }
 }
 
-async function cadastrarFilme()    // Devo fazer até a página 43 (finalizar a seção 2.19)
+function exibeAlerta(seletor, innerHTML, classesToAdd, classesToRemove, timeout)
 {
+  let alert = document.querySelector(seletor)
+  alert.innerHTML = innerHTML
+  // ... é o operador spread
+  // ele é aplicado a um array para desmembrar seus elementos, isto é, uma lista
+  alert.classList.add(...classesToAdd)
+  alert.classList.remove(...classesToRemove)
+  setTimeout(() => {
+    alert.classList.remove(...classesToAdd)
+    alert.classList.add(...classesToRemove)
+  }, timeout)
+}
+
+async function obterFilmes()
+{
+  const filmesEndPoint = "/filmes"
+  const URLcompleta = `${protocolo}${baseURL}${filmesEndPoint}`
+  const filmes = (await axios.get(URLcompleta)).data
+
+  exibirFilmes(filmes)
+}
+
+async function cadastrarFilme()
+{
+  const filmesEndPoint = "/filmes"
   //constrói a URL completa
   const URLCompleta = `${protocolo}${baseURL}${filmesEndPoint}`
 
@@ -43,28 +64,49 @@ async function cadastrarFilme()    // Devo fazer até a página 43 (finalizar a 
     const filmes = (await axios.post(URLCompleta, {titulo, sinopse})).data
   
     //limpa a tabela para preenchê-la com a coleção nova, atualizada
-    let tabela = document.querySelector('.filmes')
-    let corpoTabela = tabela.getElementsByTagName('tbody')[0]
-    corpoTabela.innerHTML = ""
-    for (let filme of filmes)
-    {
-      let linha = corpoTabela.insertRow(0)
-      let celulaTitulo = linha.insertCell(0)
-      let celulaSinopse = linha.insertCell(1)
-      celulaTitulo.innerHTML = filme.titulo
-      celulaSinopse.innerHTML = filme.sinopse
-    }
+    exibirFilmes(filmes)
   }
 
   //senão, exibe o alerta por até 2 segundos
   else
   {
-    let alert = document.querySelector('.alert')
-    alert.classList.add('show')
-    alert.classList.remove('d-none')
-    setTimeout(() => {
-      alert.classList.remove('show')
-      alert.classList.add('d-none')
-      }, 2000)      
+    exibeAlerta(".alert-filme", "Preencha todos os campos", ["show", "alert-danger"], ["d-none"], 2000)
+  }
+}
+
+async function cadastrarUsuario()
+{
+  let usuarioCadastroInput = document.querySelector('#usuarioCadastroInput')
+  let passwordCadastroInput = document.querySelector('#passwordCadastroInput')
+
+  let usuarioCadastro = usuarioCadastroInput.value
+  let passwordCadastro = passwordCadastroInput.value
+
+  usuarioCadastroInput.value = ""  
+  passwordCadastroInput.value = ""
+
+  if(usuarioCadastro && passwordCadastro)
+  {
+    let modalCadastro = bootstrap.Modal.getInstance(document.querySelector("#modalCadastro"))
+
+    try
+    {
+      const cadastroEndpoint = "/signup"
+      const URLcompleta = `${protocolo}${baseURL}${cadastroEndpoint}`
+      await axios.post(URLcompleta, {login: usuarioCadastro, password: passwordCadastro})
+
+      exibeAlerta(".alert-modal-cadastro", "Usuário cadastrado com sucesso!!!", ["show", "alert-success"], ["d-none"], 2000)
+      setTimeout(() => {modalCadastro.hide()}, 2000)
+    }
+    catch(e)
+    { 
+      exibeAlerta(".alert-modal-cadastro", "Não foi possível cadastrar o usuário!!!", ["show", "alert-danger"], ["d-none"], 2000)
+      setTimeout(() => {modalCadastro.hide()}, 2000)
+    }
+  }
+  else
+  {
+    // ativa o modal de alerta sobre preenchimento dos campos
+    exibeAlerta(".alert-modal-cadastro", "Preencha todos os campos!!!", ["show", "alert-danger"], ["d-none"], 2000)
   }
 }
